8b3d65e6e4e5a49ae259d8f439b04e30d1961d47
masa619 <oten.saber619@gmail.com>
2024-11-30 13:11:30 +0900

    113003

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,9 +1,5 @@
-import environ
-env = environ.Env()
-environ.Env.read_env()  # .envファイルから環境変数を読み込む
-
-bind = env('BIND_ADDRESS', default='unix:/Users/shipro/Projects/studyApp/studyApp.sock')
-workers = 3  # 適切な数に調整
+bind = 'unix:/home/masa619/projects/studyApp/studyApp.sock'
+workers = 1  # 適切な数に調整
 worker_class = 'sync'
 timeout = 120
 errorlog = '/mnt/ironwolf1/nas/logs/gunicorn/studyApp/error.log'

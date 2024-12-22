9ed3dde353cb15bb05f121e431e66e841d84af7e
masa619 <oten.saber619@gmail.com>
2024-12-22 16:49:13 +0900

    20241222-02

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,32 +6,36 @@ import ProtectedRoute from './contexts/ProtectedRoute';
 import Login from './components/pages/auth/Login';
 import LearningApp from './components/pages/study/QuizPage';
 import Menu from './components/pages/menu/Menu';
+import NotFound from './components/pages/NotFound'; // 404ページ用コンポーネント
 
 function App() {
-    return (
-        <SessionProvider>
-            <Routes>
-                <Route path="/" element={<Login />} />
-                <Route path="/login" element={<Login />} />
-                <Route
-                    path="/select"
-                    element={
-                        <ProtectedRoute>
-                            <Menu />
-                        </ProtectedRoute>
-                    }
-                />
-                <Route
-                    path="/quiz"
-                    element={
-                        <ProtectedRoute>
-                            <LearningApp />
-                        </ProtectedRoute>
-                    }
-                />
-            </Routes>
-        </SessionProvider>
-    );
+  return (
+    <SessionProvider>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/select"
+          element={
+            <ProtectedRoute>
+              <Menu />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/quiz"
+          element={
+            <ProtectedRoute>
+              <LearningApp />
+            </ProtectedRoute>
+          }
+        />
+        
+        {/* 存在しないルートをキャッチするワイルドカード */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </SessionProvider>
+  );
 }
 
 export default App;
---
+++ frontend/src/components/pages/NotFound.tsx
@@ -0,0 +1,13 @@
+// src/components/pages/NotFound.jsx
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>404 - Page Not Found</h1>
+      <p>お探しのページは見つかりませんでした。</p>
+    </div>
+  );
+};
+
+export default NotFound;
